11e75252fee38df3c5fdea00d08e76110a851290
Thomas <tw.98@hotmail.com>
2020-11-05 20:43:12 -0300

    paginas basicas listas

--- Github/safeplusDjango/settings.py
+++ Github/safeplusDjango/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 LOGIN_REDIRECT_URL = '/'
 
-LOGOUT_REDIRECT_URL = '/' # new
+LOGOUT_REDIRECT_URL = '/' 
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
