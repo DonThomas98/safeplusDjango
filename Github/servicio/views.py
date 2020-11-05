1765de868f2a1c76fd6cf36c04d830cfb931cc3f
Thomas <tw.98@hotmail.com>
2020-11-05 20:12:29 -0300

    Procedure que muestra clientes listo

--- Github/servicio/views.py
+++ Github/servicio/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,request
+from django.db import connection
+
+
+def Home(request):
+    data= {
+        'clientes':listar_clientes()
+    }
+
+    return render(request,'home.html',data)
 
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def listar_clientes():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("prc_listar_cliente",[out_cur])
+
+    lista=[]
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listar_trabajadores():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("prc_listar_trabajadores",[out_cur])
+
+    lista=[]
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+
+
