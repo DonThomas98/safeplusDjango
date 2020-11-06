448ef24166014e51a6a5245338500f2a4790e2d2
Thomas <tw.98@hotmail.com>
2020-11-06 02:22:05 -0300

    Templates nuevas con su respectivos procedures crearcliente/agregar datos a cliente

--- Github/servicio/urls.py
+++ Github/servicio/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
-from .views import Home
+from .views import Home,Crear_clientes,Crear_datos_clientes
 urlpatterns = [
     path('', views.Home, name='home'),
+    path('crear_clientes', views.Crear_clientes , name='crear_clientes'),
+    path('crear_datos_clientes', views.Crear_datos_clientes , name='crear_datos_clientes'),
+
+
+
+
 ]
--- Github/servicio/views.py
+++ Github/servicio/views.py
@@ -5,9 +5,25 @@ import cx_Oracle
 from datetime import datetime
 def Home(request):
     data= {
-        'clientes':listar_clientes()
+        'clientes':listar_clientes(),
+        'datosclientes':listar_datos_clientes(),
     }
+    ##agregar_datos_clientes(123456789,150000,28,141) funcional
     ##agregar_clientes('perroson','elguason','Juan','Aguilera','jg@gmail.com',)
+    if request.method == 'POST':
+        rut =request.POST.get('rut')
+        sueldo =request.POST.get('sueldo')
+        edad =request.POST.get('edad')
+        userid =request.POST.get('userid')
+        agregar_datos_clientes(rut,sueldo,edad,userid)
+       
+
+
+    return render(request,'home.html',data)
+
+
+def Crear_clientes(request):
+
     if request.method == 'POST':
         password =request.POST.get('password')
         usuario =request.POST.get('usuario')
@@ -17,7 +33,25 @@ def Home(request):
         agregar_clientes(password,usuario,nombre,apellido,correo)
        
 
-    return render(request,'home.html',data)
+    return render(request,'nuevo_cliente.html')
+
+def Crear_datos_clientes(request):
+    data= {
+        'datosclientes':listar_datos_clientes(),
+    }
+    if request.method == 'POST':
+        rut =request.POST.get('rut')
+        sueldo =request.POST.get('sueldo')
+        edad =request.POST.get('edad')
+        userid =request.POST.get('userid')
+        agregar_datos_clientes(rut,sueldo,edad,userid)
+       
+    return render(request,'nuevo_datos_cliente.html',data)
+
+
+
+
+
 
 
 def listar_clientes():
@@ -26,6 +60,18 @@ def listar_clientes():
     out_cur = django_cursor.connection.cursor()
 
     cursor.callproc("prc_listar_cliente",[out_cur])
+
+    lista=[]
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listar_datos_clientes():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("prc_listar_id_clientes",[out_cur])
 
     lista=[]
     for fila in out_cur:
@@ -49,3 +95,9 @@ def agregar_clientes(password,usuario,nombre,apellido,correo):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     cursor.callproc("prc_insertar_cliente",[password,usuario,nombre,apellido,correo])
+
+def agregar_datos_clientes(rut,sueldo,edad,userid):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    cursor.callproc("prc_insertar_datos_cliente",[rut,sueldo,edad,userid])
+
