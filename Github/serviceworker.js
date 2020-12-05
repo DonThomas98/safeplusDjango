4dcaa1cceeefa6ddb461a96dc751c3b36c37f3e3
Thomas <tw.98@hotmail.com>
2020-12-05 01:40:58 -0300

    Avance en notificaciones Push

--- Github/serviceworker.js
+++ Github/serviceworker.js
@@ -63,3 +63,34 @@ self.addEventListener('fetch', function(event) {
 
 
 
+//CODIGO NOTIFICACION PUSHU
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyASTMlsCxMMW2a72f9FjWrpZxjPkBy8grg",
+  authDomain: "safeplus-5e183.firebaseapp.com",
+  projectId: "safeplus-5e183",
+  storageBucket: "safeplus-5e183.appspot.com",
+  messagingSenderId: "763838870066",
+  appId: "1:763838870066:web:6fff29d33a5cf267a7422b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let messaging= firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload){
+
+  let tittle='Titulo de la notificacionu';
+
+  let options={
+
+    body:'mensaje del body',
+    icon:'static/images/safe.jpg'
+
+  }
+
+  self.registration.showNotification(tittle,options)
+
+});
